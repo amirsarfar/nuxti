55e83f40c2f8b3a35fa6a5e8d01e6f60a1493683
Zero Att <amirsarfar@gmail.com>
2021-06-18 20:17:50 +0430

    added and tested axios with dyno-cms project

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <a-container>
         <a-row>
+          <div v-for="type in types" :key="type.id" class="flex">
+            <div>{{type.title}}</div>
+          </div>
           <button class="btn btn-blue">sadsds</button>
         </a-row>
   </a-container>
@@ -12,7 +15,24 @@ export default {
     components:{
         AContainer,
         ARow,
+    },
+    data(){
+      return{
+        types: [],
+      }
+    },
+    mounted(){
+      this.fetchSomething();
+    },
+    methods: {
+      async fetchSomething() {
+        const ip = await this.$axios.$get('http://127.0.0.1:8000/api/v1/types')
+        // this.ip = ip
+        console.log(ip);
+        this.types = ip;
+      }
     }
+
 }
 </script>
 
